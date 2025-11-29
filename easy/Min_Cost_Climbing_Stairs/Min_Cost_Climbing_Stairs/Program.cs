b82bc1adb987507b07f3875a29c6026553f475e0
Novokrono <kirillgerman5844@gmail.com>
2025-11-29 12:35:52 +0200

    Add new leetcode solution

---
+++ easy/Min_Cost_Climbing_Stairs/Min_Cost_Climbing_Stairs/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Min_Cost_Climbing_Stairs
+{
+    internal class Program
+    {
+        public class Solution
+        {
+            public int MinCostClimbingStairs(int[] cost)
+            {
+                int length = cost.Length, current = cost[1], previous = cost[0];
+
+                for (int i = 2; i < length; i++)
+                {
+                    (current, previous) = (cost[i] + Math.Min(current, previous), current);
+                }
+                return Math.Min(current, previous);
+            }
+        }
+    }
+}
