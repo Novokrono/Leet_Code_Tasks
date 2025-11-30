11b24630eebbcac5f78737c6fe65c2f1ac87927e
Novokrono <136171065+Novokrono@users.noreply.github.com>
2025-11-30 09:19:33 +0200

    Add files via upload

---
+++ medium/881. Boats_to_Save_People/Boats_to_Save_People/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Boats_to_Save_People
+{
+    internal class Program
+    {
+        public class Solution
+        {
+
+            public int NumRescueBoats(int[] people, int limit)
+            {
+
+                Array.Sort(people);
+                int left = 0;                     // light
+                int right = people.Length - 1;    // heavy
+                int boats = 0;
+
+                while (left <= right)
+                {
+                    // we're trying to seat the light one together with the heavy one
+                    if (people[left] + people[right] <= limit)
+                    {
+                        left++;   // лёгкий сел
+                    }
+
+                    right--;      // the heavy one will always float away
+                    boats++;      // the boat left
+                }
+
+                return boats;
+            }
+        }
+    }
+}
